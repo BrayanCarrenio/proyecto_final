a42fa229abc267ee5d2b9ee1c892a29f5255f6d0
Brey <brayan26camilo26@gmail.com>
2024-11-01 15:51:29 -0500

    ajuste para el uso de los scripts

--- public/inicio_sesion.js
+++ public/inicio_sesion.js
@@ -1,31 +1,5 @@
 //funcion asyncronica para enlazar todos los elementos, guarda en constante para su posterior manipulación
 document.addEventListener('DOMContentLoaded', ()=>{ 
-    async function handleRegister(e){
-        e.preventDefault();
-        const username = document.getElementById('usuario').value;
-        const email = document.getElementById('correo').value;
-        const password =  document.getElementById('contraseña').value;
-
-        try{
-            const res = await fetch('http://localhost:5000/api/auth/register',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({username, email, password})
-            });
-            const data = await res.json();
-            if(res.status == 200){
-                alert('Registro Exitoso');
-            }else{
-                alert(`Error al registrar ${data.msg}`);
-            }
-        }catch(error){
-            console.error(error);
-            alert('Error de registro de usuario');
-        }
-    }
-
     async function handleLogin(e){
         e.preventDefault();
         const email = document.getElementById('correo_iniciar_sesion').value;
@@ -50,7 +24,5 @@ document.addEventListener('DOMContentLoaded', ()=>{
             alert('Error de inicio de sesión');
         }
     }
-
-    document.getElementById('registro').addEventListener('submit', handleRegister);
     document.getElementById('inicio_de_sesion').addEventListener('submit', handleLogin);
 })
---
+++ public/registro.js
@@ -0,0 +1,29 @@
+//funcion asyncronica para enlazar todos los elementos, guarda en constante para su posterior manipulación
+document.addEventListener('DOMContentLoaded', ()=>{ 
+    async function handleRegister(e){
+        e.preventDefault();
+        const username = document.getElementById('usuario').value;
+        const email = document.getElementById('correo').value;
+        const password =  document.getElementById('contraseña').value;
+
+        try{
+            const res = await fetch('http://localhost:5000/api/auth/register',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username, email, password})
+            });
+            const data = await res.json();
+            if(res.status == 200){
+                alert('Registro Exitoso');
+            }else{
+                alert(`Error al registrar ${data.msg}`);
+            }
+        }catch(error){
+            console.error(error);
+            alert('Error de registro de usuario');
+        }
+    }
+    document.getElementById('registro').addEventListener('submit', handleRegister);
+})
