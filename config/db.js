2840dfe0b2355b91b60fec03cbebe167619ebb27
Brey <brayan26camilo26@gmail.com>
2024-10-31 19:55:13 -0500

    preaparación para db

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const {Sequelize} = require ('sequelize');
+
+
+const sequelize = new Sequelize ('userauth', 'root', '',{
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+const connectDB = async() =>{
+    try{
+        await sequelize.authenticate()
+        console.log('Connected to database')
+    }catch (error){
+        console.error('Error connecting to database', error)
+    }
+}
+
+module.exports = {sequelize, connectDB}
+//npm init -y
+//npm i express/sequelize/dotenv
