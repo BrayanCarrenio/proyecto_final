47d5b7b10ccc0f7da93ef91e12ebcf0b1ec346cd
Brey <brayan26camilo26@gmail.com>
2024-11-06 19:22:17 -0500

    redireccionamiento

--- public/ventanas.js
+++ public/ventanas.js
@@ -5,3 +5,8 @@ const abrir_ventana = document.getElementById("registro_formulario")
 abrir_ventana.addEventListener("click", function(){
     ventana_confirmar_registro.style.display = "block";
 });
+
+//redirecionamiento pagina principal
+function redirigirPrincipal(){
+    window.location.href = '../16_pagina_principal/index.html';
+}
