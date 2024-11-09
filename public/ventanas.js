943badf9f0ca17bf806ef145df07b574c53c9f0c
Brey <brayan26camilo26@gmail.com>
2024-11-08 19:35:51 -0500

    a

--- public/ventanas.js
+++ public/ventanas.js
@@ -1,4 +1,4 @@
-//ventana emergente para registro
+//ventana emergente para la interfaz del registro
 const ventana_confirmar_registro = document.getElementById("aviso_registro")
 const abrir_ventana = document.getElementById("registro_formulario")
 
@@ -6,7 +6,9 @@ abrir_ventana.addEventListener("click", function(){
     ventana_confirmar_registro.style.display = "block";
 });
 
-//redirecionamiento pagina principal
+//redirecionamiento pagina principal desde el inicio de sesión
 function redirigirPrincipal(){
     window.location.href = '../16_pagina_principal/index.html';
 }
+
+//ventana emergente para la interfaz del evento en la pag. principal
